7e7fc6c1795040453efe15e9828a366e01d79ffb
MCE18161675 <mce18161675@belfastmet.ac.uk>
2018-12-07 10:24:36 +0000

    uploading programme which prints times tables in console

---
+++ Practice-04-TimesTables/src/main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class main {
+
+	public main() {
+	
+	}
+
+	public static void main(String[] args) {
+		//Initialising
+		int num1;
+		int num2;
+		int answer;
+		System.out.println("Printing Your Times Tables");
+		
+	
+		//Counting
+		//First Loop the first number being multiplied
+		for(num1=1; num1<=12; num1++) {
+			System.out.println("");
+			System.out.println("The "+num1+ " times tables.");
+			System.out.println("");
+			//Second loop the second number being multiplied
+			for(num2=1; num2<=12; num2++) {
+				answer = num1 * num2;
+				System.out.println(num1+" x "+num2+" = "+answer);
+				
+			}
+			
+			
+;
+		}
+	}
+
+	}
+
+
